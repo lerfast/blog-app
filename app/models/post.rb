be2485dcd69fefc8c11cbf0ed44a7278c29c34f2
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-25 14:45:31 -0500

    Adding code for exercise

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  
+  after_save :update_posts_counter
+  after_destroy :update_posts_counter
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+  
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
-end
+    has_many :posts, foreign_key: 'author_id'
+    has_many :comments
+    has_many :likes
+  
+    # Método para obtener las 3 publicaciones más recientes
+    def three_most_recent_posts
+      posts.order(created_at: :desc).limit(3)
+    end
+    
+    # Otros métodos adicionales pueden ir aquí
+  end
+  

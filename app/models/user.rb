fcc04d9b265b7b849d77b028998866069d215d89
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-25 14:47:17 -0500

    Linters!!!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-    has_many :posts, foreign_key: 'author_id'
-    has_many :comments
-    has_many :likes
-  
-    # Método para obtener las 3 publicaciones más recientes
-    def three_most_recent_posts
-      posts.order(created_at: :desc).limit(3)
-    end
-    
-    # Otros métodos adicionales pueden ir aquí
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
+
+  # Método para obtener las 3 publicaciones más recientes
+  def three_most_recent_posts
+    posts.order(created_at: :desc).limit(3)
   end
-  
+
+  # Otros métodos adicionales pueden ir aquí
+end

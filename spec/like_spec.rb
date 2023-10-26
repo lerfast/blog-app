580a3e42f19e7714c53457c8b861b3f6c288c12a
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-26 16:16:54 -0500

    Linters!!!

--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-    let!(:user) { User.create!(name: "Test User", posts_counter: 0) }
-    let!(:post) { Post.create!(title: "Test Post", author: user, comments_counter: 0, likes_counter: 0) }
+  let!(:user) { User.create!(name: 'Test User', posts_counter: 0) }
+  let!(:post) { Post.create!(title: 'Test Post', author: user, comments_counter: 0, likes_counter: 0) }
 
 
   it 'increments the likes counter when created' do
-    expect { Like.create(user: user, post: post) }.to change { post.reload.likes_counter }.by(1)
+    expect { Like.create(user:, post:) }.to change { post.reload.likes_counter }.by(1)
   end
 
   it 'decrements the likes counter when destroyed' do
-    like = Like.create(user: user, post: post)
+    like = Like.create(user:, post:)
     expect { like.destroy }.to change { post.reload.likes_counter }.by(-1)
   end
 end

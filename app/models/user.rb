fad7aff06a9d3561c36b911452a3a75b0961b3b9
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-26 15:31:37 -0500

    Adding validate for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  # Método para obtener las 3 publicaciones más recientes
+  
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
 
-  # Otros métodos adicionales pueden ir aquí
+ 
 end

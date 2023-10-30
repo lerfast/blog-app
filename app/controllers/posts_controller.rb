68b6b62074cfa9d960ae245bc655e09798ba7b6e
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-30 12:58:21 -0500

    Update posts_controller.rb
    Code for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
-end
+    def index
+      @user = User.find(params[:user_id])
+      @posts = @user.posts
+    end
+  
+    def show
+      @post = Post.find(params[:id])
+    end
+  end
+  

80c916d643131844f24f08bbcd8a209da7d26254
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-30 14:04:41 -0500

    Finishing coding for exercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,15 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  before_validation :set_default_posts_counter
+
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def set_default_posts_counter
+    self.posts_counter ||= 0
   end
 end

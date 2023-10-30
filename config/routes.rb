b39b466a89bea308807679867e7efefdbc1db849
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-30 14:24:27 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get 'users', to: 'users#index'    
-  get 'users/:id', to: 'users#show' 
+  get 'users', to: 'users#index', as: :users  
+  get 'users/:id', to: 'users#show', as: :user 
 
-  
-  get 'users/:user_id/posts', to: 'posts#index'  
-  get 'posts/:id', to: 'posts#show'             
+  get 'users/:user_id/posts', to: 'posts#index', as: :user_posts  
+  get 'posts/:id', to: 'posts#show', as: :user_post               
 end

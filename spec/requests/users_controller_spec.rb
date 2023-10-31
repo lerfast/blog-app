17d470942966b6ba138b0596b3b27fe13a0afe99
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-10-31 17:53:26 -0500

    running linters

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "UsersController", type: :request do
+RSpec.describe 'UsersController', type: :request do
   let!(:user) { User.create!(name: 'Test User') }
 
-  describe "GET /index" do
-    it "returns a successful response" do
+  describe 'GET /index' do
+    it 'returns a successful response' do
       get users_path
       expect(response).to have_http_status(:success)
     end
 
-    it "renders the index template" do
+    it 'renders the index template' do
       get users_path
       expect(response).to render_template(:index)
     end
@@ -20,13 +20,13 @@ RSpec.describe "UsersController", type: :request do
     end
   end
 
-  describe "GET /show" do
-    it "returns a successful response" do
+  describe 'GET /show' do
+    it 'returns a successful response' do
       get user_path(user)
       expect(response).to have_http_status(:success)
     end
 
-    it "renders the show template" do
+    it 'renders the show template' do
       get user_path(user)
       expect(response).to render_template(:show)
     end

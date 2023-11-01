fef174fce3b01b5f23ec80399af51f5f52c983c3
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-11-01 13:38:46 -0500

    Adding code for exercise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,59 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:index, :new, :create, :destroy]
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
   end
 
   def show
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build
+  end
+  
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.build(post_params)
+    if @post.save
+      redirect_to post_path(@post), notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+  
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to user_posts_path(@user), notice: 'Post was successfully destroyed.'
+  end
+
+  private
+
+  def set_post
     @post = Post.find(params[:id])
   end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get 'users', to: 'users#index', as: :users  
-  get 'users/:id', to: 'users#show', as: :user 
-
-  get 'users/:user_id/posts', to: 'posts#index', as: :user_posts  
-  get 'posts/:id', to: 'posts#show', as: :user_post               
+  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy], shallow: true
+  end
+  root 'users#index'
 end

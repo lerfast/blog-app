c8fc77ab493876c1cc47af1227acae7d1973ac21
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-11-08 12:32:53 -0500

    Linters!!
    Co-Authored-By: Sebastian Hernández <sebastian4hm@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,13 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # Devise parameter sanitizer para cuando se registren o actualicen datos de usuarios
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :bio])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :photo, :bio])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name photo bio])
   end
 
   private
 
-  # Método para recordar la última página visitada
   def set_referer
     session[:return_to] = request.referer if request.url != request.referer
   end
